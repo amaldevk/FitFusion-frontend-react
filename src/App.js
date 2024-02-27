7891c7ccf6b3552fc2b66bb62be8953c973f9c58
taniya-ignatius <itaniya895@gmail.com>
2024-02-28 00:01:55 +0530

    admin login success

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import UserLogin from './components/UserLogin';
+import AdminLogin from './components/AdminLogin';
 
 
 
@@ -17,7 +18,7 @@ import UserLogin from './components/UserLogin';
 function App() {
   return (
     <div>
-     
+
     </div>
   );
 }
---
+++ src/components/AdminLogin.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+const AdminLogin = () => {
+    const navigate = useNavigate()
+    const [input, setInput] = new useState(
+        {
+            "username": "",
+            "password": ""
+        }
+    )
+    const inputHandler = (event) => {
+        setInput({ ...input, [event.target.name]: event.target.value })
+    }
+    const readValues = () => {
+        console.log(input)
+        if (input.username == "admin" && input.password == "admin") {
+            navigate("/viewregistered")
+            alert("Successfully signed in")
+            setInput({
+                "username": "",
+                "password": ""
+            })
+        }
+        else {
+            alert("Invalid username or password")
+            setInput(
+                {
+                    "username": "",
+                    "password": ""
+                }
+            )
+        }
+    }
+    return (
+        <div><center>
+            <div class="bg-secondary p-3">
+                <div class="card">
+                    <div className="container">
+                        <div className="row g-3">
+                            <div class="card-body">
+                                <h2 class="card-title"><u>ADMIN LOGIN</u></h2><br></br>
+                                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                    <div className="row g-3">
+                                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                            <label htmlFor="" className="form-label">USERNAME</label>
+                                            <input type="text" className="form-control" name="username" value={input.username} onChange={inputHandler} />
+                                        </div>
+                                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                            <label htmlFor="" className="form-label">PASSWORD</label>
+                                            <input type="password" className="form-control" name="password" value={input.password} onChange={inputHandler} />
+                                        </div>
+                                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                            <button className="btn btn-primary" onClick={readValues}>LOGIN</button>
+                                        </div>
+                                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                            <center><Link to="/login" className="nav-link">Users Click Here</Link></center>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </center></div>
+    )
+}
+
+export default AdminLogin
