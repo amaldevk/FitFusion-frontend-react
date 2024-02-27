a62e6b15673c16ec9b9581dbe5200f0f330ffa9a
lakshmiibalu <lakshmibalu74@gmail.com>
2024-02-27 12:20:23 +0530

    add packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddPackages from './components/AddPackages';
 
 function App() {
   return (
     <div>
       
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AddPackages/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
