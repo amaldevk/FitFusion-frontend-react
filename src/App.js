93fbc975bbd380f885d07c6bf53cbe185cd5ee87
TaniyaMathew1 <taniyamathew2000@gmail.com>
2024-02-27 13:33:13 +0530

    viewtrainer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddPackages from './components/AddPackages';
+import ViewTrainer from './components/ViewTrainer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<AddPackages/>}/>
+        <Route path='/viewtrainers' element={<ViewTrainer/>}/>
       </Routes>
       </BrowserRouter>
     </div>
