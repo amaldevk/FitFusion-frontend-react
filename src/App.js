df3ff5260c160a9fa61338d24f28d81ab446d5f0
Mahadevan1234 <mahi17700@gmail.com>
2024-02-27 15:05:28 +0530

    registerpagedone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import RegisterPage from './components/RegisterPage';
 
 function App() {
   return (
     <div>
-      
+      <RegisterPage/>
     </div>
   );
 }
