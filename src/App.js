d97701904d9b3db5e622100a30e2fc14b3c0f23f
TaniyaMathew1 <taniyamathew2000@gmail.com>
2024-02-27 12:57:42 +0530

    viewtrainers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ViewTrainers from './components/ViewTrainers';
 
 function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/viewtrainer' element={<ViewTrainers/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
