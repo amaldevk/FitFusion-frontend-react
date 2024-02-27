4c6b30b4e9c656d2ca62c93c04d4f9c0b1ced578
taniya-ignatius <itaniya895@gmail.com>
2024-02-28 00:03:42 +0530

    admin login success

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import UserLogin from './components/UserLogin';
-import AdminLogin from './components/AdminLogin';
 
 
 
