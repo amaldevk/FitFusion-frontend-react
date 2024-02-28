78b033f6462e63bf1ea1723059c36559766f936e
Mahadevan1234 <mahi17700@gmail.com>
2024-02-28 12:09:23 +0530

    homepagebug

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import HomePage from './components/HomePage';
 import AdminLogin from './components/AdminLogin';
 import UserLogin from './components/UserLogin';
 import RegisterPage from './components/RegisterPage';
+import AddPackages from './components/AddPackages';
+import ViewPackages from './components/ViewPackages';
+import AddTrainerDetails from './components/AddTrainerDetails';
+import ViewTrainer from './components/ViewTrainer';
    
 
 
@@ -16,6 +20,10 @@ function App() {
         <Route path='/adminlogin' element={<AdminLogin/>}/>
         <Route path='/userlogin' element={<UserLogin/>}/>
         <Route path='/registeruser' element={<RegisterPage/>}/>
+        <Route path='/addpackage' element={<AddPackages/>}/>
+        <Route path='/viewpackage' element={<ViewPackages/>}/>
+        <Route path='/addtrainer' element={<AddTrainerDetails/>}/>
+        <Route path='/viewtrainer' element={<ViewTrainer/>}/>
       </Routes>
       </BrowserRouter>
     </div>
